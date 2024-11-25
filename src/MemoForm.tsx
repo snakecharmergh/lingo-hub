5c4df20fe519a2a0954542396f1e1bec43793c79
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-11-25 16:00:22 +0100

    handle the creation of a new tag and store it && nviagte to the previous page

--- src/MemoForm.tsx
+++ src/MemoForm.tsx
@@ -1,25 +1,32 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from 'react-select/creatable'
 import { MemoData, Tag } from "./App";
+import { v4 as uuidV4 } from 'uuid'
+
 
 type MemoFormProps= {
-  onSubmit: (data: MemoData)=> void
-}
+  onSubmit: (data: MemoData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]}
 
-export function MemoForm({onSubmit}: MemoFormProps) {
+export function MemoForm({onSubmit, onAddTag, availableTags}: MemoFormProps) {
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
-  const [selectedTags, setSelectedTags]= useState<Tag[]>([])
+  const [selectedTags, setSelectedTags]= useState<Tag[]>([]) //to store our tags
+  const navigate = useNavigate( )
 
   function handleSubmit(e:FormEvent) {
     e.preventDefault()
+
     onSubmit({
       title: titleRef.current!.value,
       body: markdownRef.current!.value,
-      tags: []
+      tags: selectedTags,
     })
+    //to navigate to the previous page once clicking the save button
+    navigate('..')
   }
 
   return (
@@ -36,10 +43,21 @@ export function MemoForm({onSubmit}: MemoFormProps) {
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect
+              <CreatableReactSelect // allows to create tags
+                onCreateOption= {label => {
+                  const newTag = {id: uuidV4(), label}
+                  onAddTag(newTag)
+                  setSelectedTags(prev => [...prev, newTag ])
+                }}
+                // creatableReactSelect expects for its value: a label and an id
                 value={selectedTags.map(tag => {
                   return {label:tag.label, value: tag.id}
                 })}
+                //set the options for onCreateOption of tags
+                options= {availableTags.map(tag => {
+                  return {label: tag.label, value: tag.id  }
+                })}
+                // to convert from the value that creatbleReactSelect expects to the value we are storing for our type(Tag= id and label)
                 onChange={tags => {
                   setSelectedTags(
                     tags.map(tag => {
--- src/NewMemo.tsx
+++ src/NewMemo.tsx
@@ -1,15 +1,17 @@
-import { MemoData } from "./App";
+import { MemoData, Tag } from "./App";
 import { MemoForm } from "./MemoForm";
 
 type NewMemoProps = {
   onSubmit: (data: MemoData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-export function NewMemo({onSubmit}: NewMemoProps){
+export function NewMemo({onSubmit, onAddTag,availableTags }: NewMemoProps){
   return (
     <>
       <h1 className="mb-4">New Memo</h1>
-      <MemoForm onSubmit={onSubmit} />
+      <MemoForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags}/>
     </>
   )
 }
